cfdee939360f54797d428d38c67fd55b90d92002
wanli <kanzihuang@qq.com>
2022-09-05 16:14:20 +0800

    Deploy extranet cluster by kubespray-v2.19.1

--- bin/utils.sh
+++ bin/utils.sh
@@ -3,7 +3,7 @@ inventory_dir=$(cd "$(dirname $0)/../inventory/" && pwd)
 function kubespray-run() {
   docker run --rm -it --mount type=bind,source="${inventory_dir}",dst=/inventory \
     --mount type=bind,source="${HOME}"/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
-    docker.io/kanzihuang/kubespray:v2.19.0 "$@"
+    docker.io/kanzihuang/kubespray:v2.19.1 "$@"
 }
 
 function docker-ansible() {
